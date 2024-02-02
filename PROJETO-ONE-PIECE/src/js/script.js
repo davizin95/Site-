026681f5998c7e72a187ed24c3242e393b3c8293
davizin95 <davislima5002@gmail.com>
2024-02-02 20:05:51 -0300

    PROJETO-ONE-PIECE

---
+++ PROJETO-ONE-PIECE/src/js/script.js
@@ -0,0 +1,24 @@
+
+const botoes = document.querySelectorAll('.botoes');
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desselecionarBotao();
+        desselecionarPersonagem();
+
+        botao.classList.add("selecionado");
+        personagens[indice].classList.add("selecionado")
+    });
+})
+
+function desselecionarPersonagem() {
+    personagens.forEach(personagem => {
+        personagem.classList.remove("selecionado");
+    });
+}
+function desselecionarBotao() {
+    botoes.forEach(botao => {
+        botao.classList.remove("selecionado");
+    });
+}
